ddf5506ae42b2c651c37368dc7e9c2dd475c3913
AmineSoukara <65272894+AmineSoukara@users.noreply.github.com>
2020-06-17 01:34:58 +0100

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -22,7 +22,7 @@ class MirrorStatus:
 
 
 PROGRESS_MAX_SIZE = 100 // 8
-PROGRESS_INCOMPLETE = ['□', '□', '□', '□', '□', '□', '□']
+PROGRESS_INCOMPLETE = ['░', '░', '░', '░', '░', '░', '░']
 
 SIZE_UNITS = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
 
@@ -77,10 +77,10 @@ def get_progress_bar_string(status):
     p = min(max(p, 0), 100)
     cFull = p // 8
     cPart = p % 8 - 1
-    p_str = '■' * cFull
+    p_str = '█' * cFull
     if cPart >= 0:
         p_str += PROGRESS_INCOMPLETE[cPart]
-    p_str += '░' * (PROGRESS_MAX_SIZE - cFull)
+    p_str += '▓' * (PROGRESS_MAX_SIZE - cFull)
     p_str = f"[{p_str}]"
     return p_str
 
@@ -89,11 +89,12 @@ def get_readable_message():
     with download_dict_lock:
         msg = ""
         for download in list(download_dict.values()):
-            msg += f"<b>📂 Filename :</b> <i>{download.name()}</i>"
-            msg += f"\n<b>✅ Status :</b> <code>{download.status()}</code>"
+            msg += f"📂 <b>Filename :</b> <code>{download.name()}</code>"
+            msg += f"\n✅ <b>Status :</b> <b>{download.status()}</b>"
             if download.status() != MirrorStatus.STATUS_ARCHIVING:
-                msg += f"\n<code>{get_progress_bar_string(download)}</code>\n<b>⏳ Progress :</b> <i>{download.progress()} Of {download.size()}</i>" \
-                    f"\n<b>🌪 Speed:</b> {download.speed()}\n<b>⏰ ETA :</b> {download.eta()}"
+                msg += f"\n<code>{get_progress_bar_string(download)}</code>
+                         \n<b>⏳ Progress :</b> <i>{download.progress()} 💾 Size : {download.size()}</i>" \
+                    f"\n🌪 <b>Speed:</b> {download.speed()} ⏰ ETA : {download.eta()}"
             if download.status() == MirrorStatus.STATUS_DOWNLOADING:
                 if hasattr(download, 'is_torrent'):
                     msg += f"\n<b>Peers:</b> {download.download().connections} " \
