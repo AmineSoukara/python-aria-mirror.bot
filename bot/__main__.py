85fdc6fd10e81a750d7f6ed76d252b984bc3f199
AmineSoukara <65272894+AmineSoukara@users.noreply.github.com>
2020-06-18 23:29:13 +0100

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -114,7 +114,7 @@ def main():
     dispatcher.add_handler(stats_handler)
     dispatcher.add_handler(log_handler)
     updater.start_polling()
-    LOGGER.info("Bot Started!")
+    LOGGER.info("Bot Started! ✔")
     signal.signal(signal.SIGINT, fs_utils.exit_clean_up)
 
 
