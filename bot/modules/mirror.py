2e80486b25797f340bab4f75bb0a185bce9eb052
AmineSoukara <65272894+AmineSoukara@users.noreply.github.com>
2020-06-20 19:17:42 +0100

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -45,7 +45,7 @@ class MirrorListener(listeners.MirrorListeners):
 
     def onDownloadComplete(self):
         with download_dict_lock:
-            LOGGER.info(f"Download completed: {download_dict[self.uid].name()}")
+            LOGGER.info(f"📥 Download Completed: {download_dict[self.uid].name()}")
             download = download_dict[self.uid]
             name = download.name()
             size = download.size_raw()
@@ -57,8 +57,8 @@ class MirrorListener(listeners.MirrorListeners):
                     download_dict[self.uid] = TarStatus(name, m_path, size)
                 path = fs_utils.tar(m_path)
             except FileNotFoundError:
-                LOGGER.info('File to archive not found!')
-                self.onUploadError('Internal error occurred!!')
+                LOGGER.info('File To 🗂 Archive Not Found!')
+                self.onUploadError('Internal ⚠️ Error Occurred!!')
                 return
         else:
             path = f'{DOWNLOAD_DIR}{self.uid}/{download_dict[self.uid].name()}'
@@ -93,7 +93,7 @@ class MirrorListener(listeners.MirrorListeners):
             uname = f"@{self.message.from_user.username}"
         else:
             uname = f'<a href="tg://user?id={self.message.from_user.id}">{self.message.from_user.first_name}</a>'
-        msg = f"{uname} your download has been stopped due to: {error}"
+        msg = f"{uname} <b>Your Download Has Been Stopped</b> \n<u>Due To</u> : {error}"
         sendMessage(msg, self.bot, self.update)
         if count == 0:
             self.clean()
@@ -108,15 +108,15 @@ class MirrorListener(listeners.MirrorListeners):
 
     def onUploadComplete(self, link: str):
         with download_dict_lock:
-            msg = f'<b>Filename:</b> <code>{download_dict[self.uid].name()}</code>\n\n<b>Size:</b> <i>{download_dict[self.uid].size()}</i>\n\n<b>Gdrive:</b> {link}'            
+            msg = f'🔔 𝗨𝗽𝗹𝗼𝗮𝗱𝗲𝗱 𝗧𝗼 𝗧𝗲𝗮𝗺 𝗗𝗿𝗶𝘃𝗲 🔔\n\n🗂 <u>Ｄｒｉｖｅ</u>：\n<a href="{link}">{download_dict[self.uid].name()}</a> ({download_dict[self.uid].size()})'
             LOGGER.info(f'Done Uploading {download_dict[self.uid].name()}')
             if INDEX_URL is not None:
                 share_url = requests.utils.requote_uri(f'{INDEX_URL}/{download_dict[self.uid].name()}')
                 if os.path.isdir(f'{DOWNLOAD_DIR}/{self.uid}/{download_dict[self.uid].name()}'):
                     share_url += '/'
-                msg += f'\n\n<b>Index:</b> {share_url}'
+                msg += f'\n\n🗂 <u>Ｉｎｄｅｘ</u>：\n<a href="{share_url}">{download_dict[self.uid].name()}</a> ({download_dict[self.uid].size()}) \n\n● <b>Dev :</b> <a href="https://t.me/AmineSoukara">ＤＡＭＩＥＮ ＸＭＡＲＳ</a> \n● For Accessing TeamDrive Join : \n○ Our <a href="https://groups.google.com/d/forum/Damien-Xmars">Google Group</a>. \n● Dont Share Any Links To Public'
             if self.tag is not None:
-                msg += f'\ncc: @{self.tag}'
+                msg += f'\n○ <b>User :</b> @{self.tag}'
             try:
                 fs_utils.clean_download(download_dict[self.uid].path())
             except FileNotFoundError:
@@ -178,7 +178,7 @@ def _mirror(bot, update, isTar=False):
     else:
         tag = None
     if not bot_utils.is_url(link) and not bot_utils.is_magnet(link):
-        sendMessage('No download source provided', bot, update)
+        sendMessage('😶 <b>No Download Source Provided.</b> 👉 Use : /Help', bot, update)
         return
 
     try:
@@ -188,7 +188,7 @@ def _mirror(bot, update, isTar=False):
     listener = MirrorListener(bot, update, isTar, tag)
     aria = aria2_download.AriaDownloadHelper(listener)
     aria.add_download(link, f'{DOWNLOAD_DIR}/{listener.uid}/')
-    sendStatusMessage(update, bot)
+    sendMessage('✅ <b>Your Url Added. Check /status 📊</b>', bot, update)
     if len(Interval) == 0:
         Interval.append(setInterval(DOWNLOAD_STATUS_UPDATE_INTERVAL, update_all_messages))
 
