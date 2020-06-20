ae2871f6939a4fe144b7aec284066f62667399fc
AmineSoukara <65272894+AmineSoukara@users.noreply.github.com>
2020-06-20 22:46:33 +0100

    Update clone.py

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -11,13 +11,13 @@ def cloneNode(update,context):
     args = update.message.text.split(" ",maxsplit=1)
     if len(args) > 1:
         link = args[1]
-        msg = sendMessage(f"Cloning: <code>{link}</code>",context.bot,update)
+        msg = sendMessage(f"♻️ Cloning : <code>{link}</code>",context.bot,update)
         gd = GoogleDriveHelper()
         result = gd.clone(link)
         deleteMessage(context.bot,msg)
         sendMessage(result,context.bot,update)
     else:
-        sendMessage("Provide G-Drive Shareable Link to Clone.",bot,update)
+        sendMessage("⏳ Provide G-Drive Shareable Link To Clone.",bot,update)
 
 clone_handler = CommandHandler(BotCommands.CloneCommand,cloneNode,filters=CustomFilters.authorized_chat | CustomFilters.authorized_user)
 dispatcher.add_handler(clone_handler)
